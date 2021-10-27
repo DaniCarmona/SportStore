6b30208da4076e0e52ad27018dc3b2f52d1e9679
Daniel Carmona <carmona240101@hotmail.com>
2021-10-27 10:32:18 +0100

    Created interface for Sports Store Repository and a fake repository

---
+++ Models/FakeSportStorRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class FakeSportStorRepository : ISportsStoreRepository
+    {
+        public IEnumerable<Product> Products => new List<Product> { 
+            new Product
+            {
+                Name="Football",
+                Price= 25,
+            },
+            new Product
+            {
+                Name="Surfboard",
+                Price=179
+            },
+            new Product
+            {
+                Name="Running Shoes",
+                Price=95
+            }
+            
+        };
+        
+    }
+}
